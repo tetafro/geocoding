6a08a5fea75c3553ae8ab6d7ed7a50b0d6124595
Denis Krivak <d.krivak@fasten.com>
2018-05-07 11:20:37 +0300

    Fix coordinate system problems on copy.

--- data/copy.sql
+++ data/copy.sql
@@ -1,13 +1,19 @@
 INSERT INTO place (osm_id, osm_type, name, coordinate)
 SELECT osm_id, all_tags -> 'type',
     COALESCE(NULLIF(name_ru, ''), name),
-    POINT(ST_X(geometry), ST_Y(geometry))
+    POINT(
+        ST_X(ST_Transform(geometry, 4326)),
+        ST_Y(ST_Transform(geometry, 4326))
+    )
 FROM import.osm_point;
 
 INSERT INTO place (osm_id, osm_type, name, coordinate)
 SELECT osm_id, all_tags -> 'type',
     COALESCE(NULLIF(name_ru, ''), name),
-    POINT(ST_X(geometry), ST_Y(geometry))
+    POINT(
+        ST_X(ST_Transform(geometry, 4326)),
+        ST_Y(ST_Transform(geometry, 4326))
+    )
 FROM (
     SELECT osm_id, all_tags, name, name_ru,
         ST_Centroid(geometry) AS geometry
@@ -17,7 +23,10 @@ FROM (
 INSERT INTO place (osm_id, osm_type, name, coordinate)
 SELECT osm_id, all_tags -> 'type',
     COALESCE(NULLIF(name_ru, ''), name),
-    POINT(ST_X(geometry), ST_Y(geometry))
+    POINT(
+        ST_X(ST_Transform(geometry, 4326)),
+        ST_Y(ST_Transform(geometry, 4326))
+    )
 FROM (
     SELECT osm_id, all_tags, name, name_ru,
         ST_Centroid(geometry) AS geometry
